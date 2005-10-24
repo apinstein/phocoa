7de47a6c4830b53aeba1697157a7904647fd712c
Alan Pinstein <apinstein@mac.com>
2005-10-24 20:54:10 +0000

    fix bug - function has wrong name
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@141 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/smarty/plugins/function.WFPaginatorSortLink.php
+++ phocoa/smarty/plugins/function.WFPaginatorSortLink.php
@@ -9,16 +9,16 @@
  */ 
     
 /** 
- *  Smarty plugin to include a WFTextField.
+ *  Smarty plugin to include a WFPaginatorSortLink.
  *  
  *  Smarty Params:
- *  id - The id of the WFTextField to use.
+ *  id - The id of the WFPaginatorSortLink to use.
  *
  *  @param array The params from smarty tag.
  *  @param object WFSmarty object of the current tpl.
  *  @return string The rendered HTML.
  */
-function smarty_function_WFTextField($params, &$smarty)
+function smarty_function_WFPaginatorSortLink($params, &$smarty)
 {
     return $smarty->getCurrentWidget($params)->render();
 }
