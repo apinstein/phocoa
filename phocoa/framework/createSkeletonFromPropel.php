3ee7137c5ad416362be0aeffa58143725032b13b
Alan Pinstein <apinstein@mac.com>
2005-11-09 23:17:16 +0000

    update skeleton generator to create delete confirmation page update skeleton generator to use WFMessageBox for consistency and good looks. update skeleton generator to include stubs for security checks
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@156 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/createSkeletonFromPropel.php
+++ phocoa/framework/createSkeletonFromPropel.php
@@ -1,12 +1,19 @@
 <?php
 
 require_once 'Console/Getopt.php';
-require_once '/Users/alanpinstein/dev/sandbox/phocoadev/phocoadev/conf/webapp.conf';
-require_once 'framework/WFWebApplication.php';
-require_once 'framework/util/PHPArrayDumper.php';
+require_once('propel/engine/database/model/NameFactory.php');
+require_once('propel/engine/database/model/NameGenerator.php');
+
+// set up defaults
+
+// pull env
+if ($phocoaConfFile = getenv('PHOCOA_PROJECT_CONF'))
+{
+    $__config['phocoaConfFile'] = $phocoaConfFile;
+}
 
 $shortopts = "";
-$longopts = array('objectName=', 'outputDir==', 'confFile=', 'databaseName=', 'pageName=', 'pageType=');
+$longopts = array('phocoaConfFile=', 'tableName=', 'propelOutputDir=', 'propelConfFile=', 'databaseName=', 'pageName=', 'pageType=');
 
 $cgo = new Console_Getopt;
 $args = $cgo->readPHPArgv();
@@ -25,13 +32,16 @@ foreach ($options[0] as $optInfo) {
     $optValue = $optInfo[1];
     //print "Checking $optName :: $optValue\n";
     switch ($optName) {
-        case '--objectName':
-            $__config['objectName'] = $optValue;
+        case '--phocoaConfFile':
+            $__config['phocoaConfFile'] = $optValue;
             break;
-        case '--confFile':
+        case '--tableName':
+            $__config['tableName'] = $optValue;
+            break;
+        case '--propelConfFile':
             $__config['propelConfFile'] = $optValue;
             break;
-        case '--outputDir':
+        case '--propelOutputDir':
             $__config['propelOutputDir'] = $optValue;
             break;
         case '--databaseName':
@@ -45,14 +55,24 @@ foreach ($options[0] as $optInfo) {
             break;
     }
 }
+// use defaults
 // assert params
-if (!isset($__config['objectName'])) throw( new Exception("objectName must be specified.") );
-if (!isset($__config['propelConfFile'])) throw( new Exception("confFile must be specified.") );
-if (!isset($__config['propelOutputDir'])) throw( new Exception("outputDir must be specified.") );
-if (!isset($__config['propelDatabaseName'])) throw( new Exception("databaseName must be specified.") );
-if (!isset($__config['pageType'])) throw( new Exception("pageType must be specified.") );
-if (!isset($__config['phocoaPageName'])) throw( new Exception("phocoaPageName must be specified.") );
+if (empty($__config['tableName'])) throw( new Exception("tableName must be specified.") );
+if (empty($__config['phocoaConfFile'])) throw( new Exception("phocoaConfFile must be specified.") );
+if (empty($__config['propelConfFile'])) throw( new Exception("propelConfFile must be specified.") );
+if (empty($__config['propelOutputDir'])) throw( new Exception("propelOutputDir must be specified. This is the directory that propel generates all classes in, without the database name. Same as the build file for propel.") );
+if (empty($__config['propelDatabaseName'])) throw( new Exception("databaseName must be specified.") );
+if (empty($__config['pageType'])) throw( new Exception("pageType must be specified.") );
+if (!isset($__config['phocoaPageName']))
+{
+    $__config['phocoaPageName'] = $__config['pageType'];
+}
 if (!in_array($__config['pageType'], SkeletonDumperPropel::pageTypes())) throw( new Exception("pageType must be one of: " . join(',', SkeletonDumperPropel::pageTypes())) );
+
+// include the web app to bootstrap phocoa
+require_once $__config['phocoaConfFile'];
+require_once 'framework/WFWebApplication.php';
+require_once 'framework/util/PHPArrayDumper.php';
 
 // set up ini path to include the propel dir
 ini_set('include_path', ini_get('include_path') . ":{$__config['propelOutputDir']}");
@@ -60,13 +80,14 @@ ini_set('include_path', ini_get('include_path') . ":{$__config['propelOutputDir'
 require_once($__config['propelConfFile']);
 // load desired object file
 $propelClassesDir = $__config['propelOutputDir'] . "/{$__config['propelDatabaseName']}";
-require_once( $propelClassesDir . '/' . $__config['objectName'] . '.php');
+$propelObjectName = NameFactory::generateName(NameFactory::PHP_GENERATOR, array($__config['tableName'], NameGenerator::CONV_METHOD_PHPNAME));
+require_once( $propelClassesDir . '/' . $propelObjectName . '.php');
 // start up propel; this will load the database map including info for all included files
 Propel::init($__config['propelConfFile']);
 // load databaseMap into inst var
 $propelDBMap = Propel::getDatabaseMap($__config['propelDatabaseName']);
 
-$sdp = new SkeletonDumperPropel($propelDBMap, $__config['objectName']);
+$sdp = new SkeletonDumperPropel($propelDBMap, $__config['tableName']);
 switch ($__config['pageType']) {
     case 'edit':
         $sdp->updateEditPage($__config['phocoaPageName']);
@@ -89,8 +110,8 @@ class SkeletonDumperPropel
     function __construct($dbMap, $tableName)
     {
         $this->tableName = $tableName;
-        $this->sharedInstanceID = $this->tableName;
-        $this->className = ucfirst($this->sharedInstanceID);
+        $this->className = NameFactory::generateName(NameFactory::PHP_GENERATOR, array($tableName, NameGenerator::CONV_METHOD_PHPNAME));
+        $this->sharedInstanceID = $this->className;
         $this->dbMap = $dbMap;
         $this->singlePrimaryKey = NULL;
     }
@@ -217,7 +238,7 @@ class SkeletonDumperPropel
         }
         else
         {
-            print "Template file already exists, so it will not be overwritten. Here is the template code for the table:\n$tpl\n";
+            print "\nTemplate file already exists, so it will not be overwritten. Here is the template code for the table:\n$tpl\n";
         }
 
         // suggested module code
@@ -246,7 +267,7 @@ class SkeletonDumperPropel
         }
         else
         {
-            // eventually call createPage script
+            // eventually call createPage script?
             $pageInstances = array();
         }
         $configFile = $pageName . '.config';
@@ -257,19 +278,19 @@ class SkeletonDumperPropel
         }
         else
         {
-            // eventually call createPage script
+            // eventually call createPage script?
             $pageConfig = array();
         }
         
         // set up the form
-        $formID = $this->tableName . 'Form';
+        $formID = $this->className . 'Form';
         // finish form
         if (!isset($pageInstances[$formID]))
         {
             $pageInstances[$formID] = array('class' => 'WFForm', 'children' => array());
         }
         // deal with all of the columns
-        $tpl = "{WFShowErrors}\n{WFForm id=\"{$formID}\"}\n" . '<table border="1" cellpadding="3" cellspacing="0">' . "\n";
+        $tpl = "{WFLabel id=\"statusMessage\"}\n{WFShowErrors}\n{WFForm id=\"{$formID}\"}\n" . '<table border="1" cellpadding="3" cellspacing="0">' . "\n";
         foreach ($this->dbMap->getTable($this->tableName)->getColumns() as $col) {
             $displayInLayout = true;
             $widgetID = strtolower(substr($col->getPhpName(), 0, 1)) . substr($col->getPhpName(), 1);
@@ -316,6 +337,11 @@ class SkeletonDumperPropel
                 $tpl .= "    {{$class} id=\"{$widgetID}\"}\n";
             }
 
+        }
+        // status message
+        if (!isset($pageInstances['statusMessage']))
+        {
+            $pageInstances['statusMessage'] = array('class' => 'WFMessageBox', 'children' => array());
         }
         // submit buttons
         if (!isset($pageInstances[$formID]['children']['new']))
@@ -383,12 +409,20 @@ class SkeletonDumperPropel
         }
         else
         {
-            print "Template file already exists, so it will not be overwritten. Here is the template code for the table:\n$tpl\n";
+            print "\nTemplate file already exists, so it will not be overwritten. Here is the template code for the table:\n$tpl\n";
         }
 
         // suggested module code
         $deleteSuccessPageName = 'deleteSuccess';
+        $confirmDeletePageName = 'confirmDelete';
         print "Suggested module code:
+    // this function should throw an exception if the user is not permitted to edit (add/edit/delete) in the current context
+    function verifyEditingPermissions()
+    {
+        // example
+        // \$authInfo = WFAuthorizationManager::sharedAuthorizationManager()->authorizationInfo();
+        // if (\$authInfo->userid() != \$this->{$this->sharedInstanceID}->selection()->getUserId()) throw( new Exception(\"You don't have permission to edit {$this->className}.\") );
+    }
     function {$pageName}_ParameterList()
     {
         return array('{$this->singlePrimaryKey}');
@@ -398,16 +432,18 @@ class SkeletonDumperPropel
         if (\$params['{$this->singlePrimaryKey}'])
         {
             \$this->{$this->sharedInstanceID}->setContent(array({$this->className}Peer::retrieveByPK(\$params['{$this->singlePrimaryKey}'])));
+            \$this->verifyEditingPermission();
         }
         else
         {
             // will prepare content automatically, for new instance
         }
     }
-    function saveAgent(\$page)
+    function save{$this->className}(\$page)
     {
         try {
             \$this->{$this->sharedInstanceID}->selection()->save();
+            \$page->outlet('statusMessage')->setValue(\"{$this->className} saved successfully.\");
         } catch (Exception \$e) {
             \$page->addError( new WFError(\$e->getMessage()) );
         }
@@ -422,21 +458,161 @@ class SkeletonDumperPropel
     }
     function {$pageName}_delete_Action(\$page)
     {
+        \$this->verifyEditingPermission();
+        \$this->setupResponsePage('{$confirmDeletePageName}');
+    }
+
+    function {$confirmDeletePageName}_PageDidLoad(\$page, \$params)
+    {
+        if (\$page->hasSubmittedForm())
+        {
+            // load selection from form hidden field
+            \$this->{$this->sharedInstanceID}->setContent(array({$this->className}Peer::retrieveByPK(\$page->outlet('{$this->singlePrimaryKey}')->value())));
+        }
+        if (\$this->{$this->sharedInstanceID}->selection() === NULL) throw( new Exception(\"Could not load {$this->className} object to delete.\") );
+    }
+    function {$confirmDeletePageName}_cancel_Action(\$page)
+    {
+        \$this->setupResponsePage('{$pageName}');
+    }
+    function {$confirmDeletePageName}_delete_Action(\$page)
+    {
+        \$this->verifyEditingPermission();
         \$myObj = \$this->{$this->sharedInstanceID}->selection();
         \$myObj->delete();
         \$this->{$this->sharedInstanceID}->removeObject(\$myObj);
-        \$this->setupResponsePage('$deleteSuccessPageName');
+        \$this->setupResponsePage('{$deleteSuccessPageName}');
     }
+
         \n";
-        // write out deleteSuccess page.
-        if (!file_exists($deleteSuccessPageName . '.tpl'))
+        // write out confirmDelete page
+        if (!file_exists($confirmDeletePageName . '.tpl'))
         {
-            print "Creating {$deleteSuccessPageName}.tpl file.\n";
-            file_put_contents($deleteSuccessPageName . '.tpl', $this->className . " successfully deleted.");
+            // set up instances/config
+            $tplFile = $confirmDeletePageName . '.tpl';
+            $instanceFile = $confirmDeletePageName . '.instances';
+            if (file_exists($instanceFile))
+            {
+                include($instanceFile);
+                $pageInstances = $__instances;
+            }
+            else
+            {
+                // eventually call createPage script?
+                $pageInstances = array();
+            }
+            $configFile = $confirmDeletePageName . '.config';
+            if (file_exists($configFile))
+            {
+                include($configFile);
+                $pageConfig = $__config;
+            }
+            else
+            {
+                // eventually call createPage script?
+                $pageConfig = array();
+            }
+            $formID = $this->className . 'ConfirmDeleteForm';
+            $pageInstances[$formID] = array('class' => 'WFForm', 'children' => array());
+            $pageInstances[$formID]['children'][$this->singlePrimaryKey] = array('class' => 'WFHidden', 'children' => array());
+            $pageConfig[$this->singlePrimaryKey] = array(
+                                                        'bindings' => array(
+                                                                        'value' => array(
+                                                                                        'instanceID' => $this->sharedInstanceID,
+                                                                                        'controllerKey' => 'selection',
+                                                                                        'modelKeyPath' => $this->singlePrimaryKey
+                                                                                        )
+                                                                        )
+                                                        );
+            $pageInstances[$formID]['children']['cancel'] = array('class' => 'WFSubmit', 'children' => array());
+            $pageConfig['cancel'] = array(
+                                        'properties' => array(
+                                                            'label' => 'Cancel'
+                                                            )
+                                        );
+            $pageInstances[$formID]['children']['delete'] = array('class' => 'WFSubmit', 'children' => array());
+            $pageConfig['delete'] = array(
+                                        'properties' => array(
+                                                            'label' => 'Delete'
+                                                            )
+                                        );
+            $pageInstances['confirmMessage'] = array('class' => 'WFMessageBox', 'children' => array());
+            $pageConfig['confirmMessage'] = array(
+                                                        'bindings' => array(
+                                                                        'value' => array(
+                                                                                        'instanceID' => $this->sharedInstanceID,
+                                                                                        'controllerKey' => 'selection',
+                                                                                        'modelKeyPath' => $this->singlePrimaryKey,
+                                                                                        'options' => array(
+                                                                                                            'ValuePattern' => 'Are you sure you want to delete ' . $this->className . ' "%1%"?'
+                                                                                                            )
+                                                                                        )
+                                                                        )
+                                                        );
+            // save setup to disk
+            print "Saving updated {$confirmDeletePageName}.instances\n";
+            PHPArrayDumper::arrayToPHPFileWithArray($pageInstances, '__instances', $instanceFile);
+            print "Saving updated {$confirmDeletePageName}.config\n";
+            PHPArrayDumper::arrayToPHPFileWithArray($pageConfig, '__config', $configFile);
+            // set up tpl
+            print "Creating {$confirmDeletePageName}.tpl file.\n";
+            file_put_contents($confirmDeletePageName . '.tpl', "{* vim: set expandtab tabstop=4 shiftwidth=4 syntax=smarty: *}
+{WFMessageBox id=\"confirmMessage\"}
+{WFForm id=\"CommercialOpportunityConfirmDeleteForm\"}
+    {WFHidden id=\"opportunityId\"}
+    {WFSubmit id=\"cancel\"}{WFSubmit id=\"delete\"}
+{/WFForm}
+            ");
         }
         else
         {
-            print "Template file '$deleteSuccessPageName' already exists, so it will not be overwritten.";
+            print "\nTemplate file '$confirmDeletePageName' already exists, so it will not be overwritten.";
+        }
+
+        // write out deleteSuccess page
+        if (!file_exists($deleteSuccessPageName . '.tpl'))
+        {
+            // set up instances/config
+            $tplFile = $deleteSuccessPageName . '.tpl';
+            $instanceFile = $deleteSuccessPageName . '.instances';
+            if (file_exists($instanceFile))
+            {
+                include($instanceFile);
+                $pageInstances = $__instances;
+            }
+            else
+            {
+                // eventually call createPage script?
+                $pageInstances = array();
+            }
+            $configFile = $deleteSuccessPageName . '.config';
+            if (file_exists($configFile))
+            {
+                include($configFile);
+                $pageConfig = $__config;
+            }
+            else
+            {
+                // eventually call createPage script?
+                $pageConfig = array();
+            }
+            $pageInstances['statusMessage'] = array('class' => 'WFMessageBox', 'children' => array());
+            $pageConfig['statusMessage'] = array(
+                                                    'properties' => array(
+                                                                    'value' => $this->className . ' successfully deleted.'
+                                                                    )
+                                                );
+            // save setup to disk
+            print "Saving updated {$deleteSuccessPageName}.instances\n";
+            PHPArrayDumper::arrayToPHPFileWithArray($pageInstances, '__instances', $instanceFile);
+            print "Saving updated {$deleteSuccessPageName}.config\n";
+            PHPArrayDumper::arrayToPHPFileWithArray($pageConfig, '__config', $configFile);
+            print "Creating {$deleteSuccessPageName}.tpl file.\n";
+            file_put_contents($deleteSuccessPageName . '.tpl', "{* vim: set expandtab tabstop=4 shiftwidth=4 syntax=smarty: *}\n{WFMessageBox id=\"statusMessage\"}");
+        }
+        else
+        {
+            print "\nTemplate file '$deleteSuccessPageName' already exists, so it will not be overwritten.";
         }
     }
 }
