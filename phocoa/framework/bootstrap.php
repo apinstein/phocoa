177730319af66b53dee34e95bb5b94256d0c3be8
Alan Pinstein <apinstein@mac.com>
2006-05-02 22:45:14 +0000

    better setting for development debugging
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@300 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/bootstrap.php
+++ phocoa/framework/bootstrap.php
@@ -11,13 +11,19 @@ if (IS_PRODUCTION)
 {
     error_reporting(E_ALL);
     ini_set('display_errors', false);
-    define('WF_LOG_LEVEL', PEAR_LOG_ERR);
+    if (!defined('WF_LOG_LEVEL'))
+    {
+        define('WF_LOG_LEVEL', PEAR_LOG_ERR);
+    }
 }
 else
 {
     error_reporting(E_ALL); // | E_STRICT);
     ini_set('display_errors', true);
-    define('WF_LOG_LEVEL', PEAR_LOG_DEBUG);
+    if (!defined('WF_LOG_LEVEL'))
+    {
+        define('WF_LOG_LEVEL', PEAR_LOG_DEBUG);
+    }
 }
 
 /**
