3619a31a3bbfbcce6a20bb328cebcc894e569018
Alan Pinstein <apinstein@mac.com>
2006-10-09 05:55:12 +0000

    Major work on phing tasks for phocoa. Phocoa now includes a "phocoa" cli script to run phocoa operations like newproject or addpropel or createSkeletonFromPropel.
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@479 2d4ac85d-bafa-0310-acdd-b8e05c701c88

---
+++ phocoa/phing/tasks/WriteConfFile.php
@@ -0,0 +1,48 @@
+<?php
+require_once 'phing/Task.php';
+
+/**
+ * Task for writing out an initial build.properties file
+ * 
+ * <code>
+ * <writeConfFile>
+ * </code>
+ * 
+ * @author    Alan Pinstein <apinstein@mac.com>
+ */
+class WriteConfFile extends Task {
+    protected $ns;
+    protected $file;
+
+    public function setNamespace($ns)
+    {
+        $this->namespace = $ns;
+    }
+
+    public function setFile($file)
+    {
+        $this->file = $file;
+    }
+
+    public function main() {        
+        if (file_exists($this->file))
+        {
+            $this->log("Destination conf file: {$this->file} already exists; will not overwrite.", PROJECT_MSG_WARN);
+            return;
+        }
+
+        $text = NULL;
+        $props = $this->project->getUserProperties();
+        foreach ($props as $k => $v) {
+            print "Processing $k => $v\n";
+            if (preg_match("/^{$this->namespace}\./", $k))
+            {
+                $text .= "{$k} = {$v}\n";
+            }
+        }
+
+        $this->log("Writing conf file: {$this->file}.", PROJECT_MSG_INFO);
+        file_put_contents($this->file, $text);
+    }
+
+}
