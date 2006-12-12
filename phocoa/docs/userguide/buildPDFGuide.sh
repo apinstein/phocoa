e5a54eca9fff58d081043a45156fff350e005182
Alan Pinstein <apinstein@mac.com>
2006-12-12 06:14:28 +0000

    update for YAML and new PHOCOA Builder
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@607 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/docs/userguide/buildPDFGuide.sh
+++ phocoa/docs/userguide/buildPDFGuide.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# tools:
+# fink install docbook-xsl fop
 xsltproc file:///sw/share/xml/xsl/docbook-xsl/fo/docbook.xsl PHOCOA\ User\ Guide.xml > out.fo
-java org.apache.fop.apps.Fop out.fo "PHOCOA User Guide.pdf"
+fop out.fo "PHOCOA User Guide.pdf"
 rm out.fo
