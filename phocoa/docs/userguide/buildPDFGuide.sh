e596e5419ea33aa8edb870f94cae31126f12581f
Alan Pinstein <apinstein@mac.com>
2007-12-23 14:00:14 +0000

    improve doc producing script
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@969 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/docs/userguide/buildPDFGuide.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# tools:
-# fink install docbook-xsl fop
-fop -xml PHOCOA\ User\ Guide.xml -xsl /sw/share/xml/xsl/docbook-xsl/fo/docbook.xsl -pdf "PHOCOA User Guide.pdf"
-# old way
-# xsltproc file:///sw/share/xml/xsl/docbook-xsl/fo/docbook.xsl PHOCOA\ User\ Guide.xml > out.fo
-# fop out.fo "PHOCOA User Guide.pdf"
-# rm out.fo
---
+++ phocoa/docs/userguide/buildUserGuideFormats.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# tools:
+# fink install docbook-xsl fop
+DOCBOOK_INPUT=/Users/alanpinstein/dev/sandbox/phocoa/phocoa/docs/userguide/ug.xml
+if [ -z $1 ]; then
+    DOCBOOK_OUTPUT=.
+else
+    DOCBOOK_OUTPUT=$1
+fi
+echo $DOCBOOK_OUTPUT
+
+# PDF Version
+fop -xml $DOCBOOK_INPUT -xsl /sw/share/xml/xsl/docbook-xsl/fo/docbook.xsl -pdf $DOCBOOK_OUTPUT/PHOCOA_User_Guide.pdf
+
+# HTML Version
+docbook2html -o $DOCBOOK_OUTPUT/userguide-html $DOCBOOK_INPUT
