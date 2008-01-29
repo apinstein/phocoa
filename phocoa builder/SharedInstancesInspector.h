12418043d47b1fb2c45ebb0b74af3486088d6ecb
Alan Pinstein <apinstein@mac.com>
2008-01-29 20:13:46 +0000

    oops forgot files
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1029 2d4ac85d-bafa-0310-acdd-b8e05c701c88

---
+++ phocoa builder/SharedInstancesInspector.h
@@ -0,0 +1,16 @@
+//
+//  SharedInstancesInspector.h
+//  PHOCOA Builder
+//
+//  Created by Alan Pinstein on 1/2/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SharedInstancesInspector : NSWindowController {
+
+}
+
+@end
---
+++ phocoa builder/SharedInstancesInspector.m
@@ -0,0 +1,26 @@
+//
+//  SharedInstancesInspector.m
+//  PHOCOA Builder
+//
+//  Created by Alan Pinstein on 1/2/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "SharedInstancesInspector.h"
+
+
+@implementation SharedInstancesInspector
+
+// attempts to get the undo working for inspectors - didn't work....
+//- (NSManagedObjectContext*) managedObjectContext
+//{
+//	return [[self document] managedObjectContext];
+//}
+//
+//
+//- (NSUndoManager *) windowWillReturnUndoManager:(NSWindow *)sender
+//{
+//    return [[self managedObjectContext] undoManager];
+//}
+
+@end
