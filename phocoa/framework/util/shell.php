b30498a02bcb85ca5e2c954f548b2a20d5d46174
Alan Pinstein <apinstein@mac.com>
2008-03-10 22:07:46 +0000

    Fix readline() namespace collision.
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1128 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/util/shell.php
+++ phocoa/framework/util/shell.php
@@ -3,7 +3,23 @@
 ini_set("memory_limit", '20M');
 require_once(getenv('PHOCOA_PROJECT_CONF'));
 
-print<<<END
+class WFShell extends WFObject
+{
+    public static function readline( $prompt = '' )
+    {
+        echo $prompt;
+        $command = rtrim( fgets( STDIN ), "\n" );
+        // catch ctl-d
+        if (strlen($command) == 0)
+        {
+            exit;
+        }
+        return $command;
+    }
+
+    public static function main()
+    {
+        print<<<END
 
 Welcome to the PHOCOA shell! The PHOCOA shell is a simple interactive PHP shell for allowing you to experiment with your application interactively.
 This shell is already integrated into your current project, meaning you can instantiate objects in your project.
@@ -18,32 +34,23 @@ print_r(\$a)
 
 END;
 
-$_ = NULL;
-while (true)
-{
-    $cmd = readline('> ');
-    if (trim($cmd) == '') continue;
-    $cmd = preg_replace('/^\//', '$_', $cmd);
-    $parsedCommand = "return {$cmd};";
-    #echo "  $parsedCommand\n";
-    try {
-        $_ = eval($parsedCommand);
-        print "{$_}\n";
-    } catch (Exception $e) {
-        print "Uncaught exception with command:\n{$e}\n";
+        $_ = NULL;
+        while (true)
+        {
+            $cmd = WFShell::readline('> ');
+            if (trim($cmd) == '') continue;
+            $cmd = preg_replace('/^\//', '$_', $cmd);
+            $parsedCommand = "return {$cmd};";
+            #echo "  $parsedCommand\n";
+            try {
+                $_ = eval($parsedCommand);
+                print "{$_}\n";
+            } catch (Exception $e) {
+                print "Uncaught exception with command:\n{$e}\n";
+            }
+        }
     }
 }
 
-function readline( $prompt = '' )
-{
-    echo $prompt;
-    $command = rtrim( fgets( STDIN ), "\n" );
-    // catch ctl-d
-    if (strlen($command) == 0)
-    {
-        exit;
-    }
-    return $command;
-}
-
+WFShell::main();
 ?>
