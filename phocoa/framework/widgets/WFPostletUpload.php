52205deb8d13edb071bc61c247fea12c125cdfe8
Alan Pinstein <apinstein@mac.com>
2008-03-26 22:56:11 +0000

    - add comment
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1184 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/widgets/WFPostletUpload.php
+++ phocoa/framework/widgets/WFPostletUpload.php
@@ -16,6 +16,7 @@
  * This widget uses the {@link http://sourceforge.net/projects/postlet/ Postlet open-source Java applet}.
  * The code is GPL licensed and is distributed with PHOCOA for convenience. Full source is available from the above link.
  * The current bundled version is 0.14-alan (which is a customized version to support our needs -- we will upgraded to the latest version once our patches are accepted).
+ * @todo At some point in the future, maybe fall 2008, remove the embeddedcontent_min stuff because it should be fixed: http://www.swffix.org/devblog/?p=19
  */
 class WFPostletUpload extends WFForm implements WFUploadedFile
 {
