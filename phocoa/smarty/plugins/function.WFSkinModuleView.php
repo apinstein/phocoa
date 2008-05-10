5513f054e3e6f92bea92726cc38f7ca0445639d3
Alan Pinstein <apinstein@mac.com>
2008-05-10 14:26:55 +0000

    - Fix UI bug in error dialogs - Add responsdsToForms to WFSkinModuleView
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1255 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/smarty/plugins/function.WFSkinModuleView.php
+++ phocoa/smarty/plugins/function.WFSkinModuleView.php
@@ -15,6 +15,7 @@
  *  invocationPath - The invocationPath for the module. See {@link WFModuleInvocation}. Required.
  *  targetRootModule - If you want to customize the value of {@link WFModuleInvocation::$targetRootModule}, specify it in the param.
  *                     BOOLEAN, but remember that in smarty targetRootModule="false" passing the STRING false, so do targetRootModule=false
+ *  respondsToForms - Controls whether or not the instance of the module will "respond" to form submissions. Default: FALSE. This is useful when using WFSkinModuleView to embed functions like login or search into skins.
  *
  *  IMPORTANT!!!! WFSkinModuleView is intended for use only from skin template files. Results if used from a WFModule/Page are UNPREDICTABLE -- basically some functions will mistake the "root" module for the root of the skin rather than the root of the current WFModuleInvocation hierarchy when programmiatcally invoking WFModuleInvocations.
  *
@@ -26,6 +27,7 @@
 function smarty_function_WFSkinModuleView($params, $smarty)
 {
     if (empty($params['invocationPath'])) throw( new Exception("InvocationPath is required.") );
+    $respondsToForms = (isset($params['respondsToForms']) ? $params['respondsToForms'] : false);
 
     $rc = WFRequestController::sharedRequestController();
 
@@ -34,7 +36,7 @@ function smarty_function_WFSkinModuleView($params, $smarty)
     if (!$rootInv) return NULL;
 
     $modInvocation = new WFModuleInvocation($params['invocationPath'], $rootInv);
-    $modInvocation->setRespondsToForms(false);
+    $modInvocation->setRespondsToForms($respondsToForms);
     if (isset($params['targetRootModule']))
     {
         $modInvocation->setTargetRootModule($params['targetRootModule']);
