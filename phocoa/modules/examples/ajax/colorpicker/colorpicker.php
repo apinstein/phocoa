88369d8648ddb4e9a48988a11434ae379de53176
Alan Pinstein <apinstein@mac.com>
2008-09-21 05:50:30 +0000

    add seo
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1524 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/modules/examples/ajax/colorpicker/colorpicker.php
+++ phocoa/modules/examples/ajax/colorpicker/colorpicker.php
@@ -12,4 +12,14 @@ class module_colorpicker extends WFModule
         return 'example';
     }
 }
+
+class module_colorpicker_example
+{
+
+    function setupSkin($page, $params, $skin)
+    {
+        $skin->setTitle('YUI ColorPicker Example');
+        $skin->addMetaKeywords(array('yui colorpicker', 'yui colorpicker example', 'yui color picker', 'javascript colorpicker', 'javascript color picker'));
+    }
+}
 ?>
