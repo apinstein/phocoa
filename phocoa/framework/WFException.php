e9ea2d58b9dd327b4fba83082e35f5fa96d56529
Alan Pinstein <apinstein@mac.com>
2008-09-24 04:48:25 +0000

    refactor exception raising
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@1550 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/WFException.php
+++ phocoa/framework/WFException.php
@@ -8,11 +8,6 @@
  * @author Alan Pinstein <apinstein@mac.com>                        
  */
 
-/**
- * The default exception type.
- */
-define('WFGenericException', 'WFGenericException');
-
 /** 
  * A helper class to easily throw named exceptions.
  *
@@ -26,39 +21,4 @@ define('WFGenericException', 'WFGenericException');
  *
  * @todo Need to have all PHOCOA exceptions that are thrown switched to WFException so code can distinguish b/w PHOCOA and external exceptions.
  */
-class WFException extends Exception
-{
-    /**
-     * @var string The name of the exception.
-     */
-    protected $name;
-
-    function setName($name)
-    {
-        $this->name = $name;
-    }
-    function name()
-    {
-        return $this->name;
-    }
-
-    function __construct($message = NULL, $code = 0)
-    {
-        parent::__construct($message, $code);
-        $this->name = WFGenericException;
-    }
-
-    /**
-     *  Raise an exception of the passed type with the passed message.
-     *
-     *  @param string The name / code of the exception. Default is WFGenericException.
-     *  @throws WFException Throws a WFException of the passed type. Default is NULL.
-     */
-    static function raise($name = WFGenericException, $message = NULL)
-    {
-        $e = new WFException($message);
-        $e->setName($name);
-        throw( $e );
-    }
-}
-?>
+class WFException extends Exception {}
