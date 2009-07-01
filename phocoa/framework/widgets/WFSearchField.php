d22533a9d4479f2bf2589839fefc401971ce07a3
alanpinstein <alanpinstein@2d4ac85d-bafa-0310-acdd-b8e05c701c88>
2009-07-01 18:48:23 +0000

    - [bugfix] Fix nullPlaceholder behavior on IE. Bug would cause the text null to appear in WFSearchField instead of  on focus.
    git-svn-id: file:///Users/Shared/Development/svnroot/phocoa/trunk@2385 2d4ac85d-bafa-0310-acdd-b8e05c701c88

--- phocoa/framework/widgets/WFSearchField.php
+++ phocoa/framework/widgets/WFSearchField.php
@@ -72,7 +72,7 @@ class WFSearchField extends WFTextField
                 PHOCOA.widgets.' . $this->id . '.hasFocus = true;
                 if ($F(\'' . $this->id . '\') === \'' . $this->nullPlaceholder . '\')
                 {
-                    $(\'' . $this->id . '\').value = null;
+                    $(\'' . $this->id . '\').value = "";
                 }
                 $(\'' . $this->id . '\').removeClassName("phocoaWFSearchField_PlaceholderText");
             };
