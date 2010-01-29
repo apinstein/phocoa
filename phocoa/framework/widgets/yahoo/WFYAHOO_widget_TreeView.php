653cb9f92a1daee0e944ccd87521eb8136cfcf83
Alan Pinstein <apinstein@mac.com>
2010-01-28 22:33:02 -0500

    [bugfix] WFYAHOO_widget_TreeViewNode now works properly when there are special characters in the node IDs (/, space, etc). We were missing an encodeUriComponent()

--- phocoa/framework/widgets/yahoo/WFYAHOO_widget_TreeView.php
+++ phocoa/framework/widgets/yahoo/WFYAHOO_widget_TreeView.php
@@ -217,7 +217,7 @@ PHOCOA.widgets.{$this->id}.loadData = function(node, fnLoadComplete)
     rpc.callback.failure = PHOCOA.widgets.{$this->id}.loadDataHandleFailure;
     rpc.callback.argument = { loadComplete: fnLoadComplete, node: node };
     " . ($this->queryFieldId ? "var qVal = PHOCOA.widgets.{$this->queryFieldId}.getValue();" : NULL) . "
-    rpc.execute(encodeURIComponent(path" . ($this->queryFieldId ? ", qVal" : NULL) . "));
+    rpc.execute(path" . ($this->queryFieldId ? ", qVal" : NULL) . ");
     ";
             }
             else
