apinstein/phocoa

commit bc231ace3f350a346a5a23d49e23e15a4f3c1e71

fix bug where an extra trailing / could occur in some situations.

phocoa/framework/widgets/WFDieselNav.php
@@ -105,15 +105,16 @@ class WFDieselNav extends WFWidget
             if ($pName == $this->dpQueryStateParamName) break;
             $baseURLParams[] = $value;
         }
+        $baseURLParams = rtrim(join('/', $baseURLParams), '/');
         
         // calculate base URL for links
         if ($this->page->module()->invocation()->targetRootModule() and !$this->page->module()->invocation()->isRootInvocation())
         {
-            $this->baseURL = WWW_ROOT . '/' . $this->page->module()->invocation()->rootInvocation()->invocationPath() . '/' . join('/', $baseURLParams);
+            $this->baseURL = WWW_ROOT . '/' . $this->page->module()->invocation()->rootInvocation()->invocationPath() . $baseURLParams;
         }
         else
         {
-            $this->baseURL = WWW_ROOT . '/' . $this->page->module()->invocation()->modulePath() . '/' . $this->page->pageName() . '/' . join('/', $baseURLParams);
+            $this->baseURL = WWW_ROOT . '/' . $this->page->module()->invocation()->modulePath() . '/' . $this->page->pageName() . $baseURLParams;
         }
         return $this->baseURL;
     }

commit bada1f79c0f1ecc099ea332f4f7ddaf15026710c

fix bug introduced in bc231ace3f350a346a5a23d49e23e15a4f3c1e71 with handling of / properly for pages with leading parameters. it was all messed up (didn't have leading /, didn't account for parameters with empty values)

phocoa/RELEASE_NOTES
@@ -19,6 +19,7 @@ Version 0.3.7 - ??
   - New binding option RaisesForNotApplicableKeys (boolean) which if set to false will cause the NotApplicablePlaceholder to be used rather than throwing an exception.
   - New binding option NotApplicablePlaceholder (mixed) which will be used if RaisesForNotApplicableKeys is set to false.
   - pullBindings() now only skips processing of the current bound property (instead of all properties for the widget) when an exception is encountered.
+- [bugfix] Fixed bugs with WFDieselNav when calculating URLs for pages that have leading parameters (ie params before the required DP ones).
 
 Version 0.3.6 - 3/18/2010
 - [bugfix] WFYAHOO_widget_TreeViewNode now works properly when there are special characters in the node IDs (/, space, etc). We were missing an encodeUriComponent().

phocoa/framework/widgets/WFDieselNav.php
@@ -100,12 +100,11 @@ class WFDieselNav extends WFWidget
         if ($this->baseURL) return $this->baseURL;
 
         // calculate parameters up until our dpQueryState starts (after this point, we know what the last 4 params must be)
-        $baseURLParams = array();
+        $baseURLParams = NULL;
         foreach ($this->page()->parameters() as $pName => $value) {
             if ($pName == $this->dpQueryStateParamName) break;
-            $baseURLParams[] = $value;
+            $baseURLParams .= "/{$value}";
         }
-        $baseURLParams = rtrim(join('/', $baseURLParams), '/');
         
         // calculate base URL for links
         if ($this->page->module()->invocation()->targetRootModule() and !$this->page->module()->invocation()->isRootInvocation())