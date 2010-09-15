b0231985c9c5d1c6c9278f3e3896bf2ccbcc313a
Alan Pinstein <apinstein@mac.com>
2010-09-15 17:13:49 -0400

    Add mobile browser detection infrastructure

--- phocoa/framework/WFRequestController.php
+++ phocoa/framework/WFRequestController.php
@@ -166,7 +166,7 @@ class WFRequestController extends WFObject
             if (isset($_REQUEST[WFRPC::PARAM_ENABLE_AJAX_IFRAME_RESPONSE_MODE]) && $_REQUEST[WFRPC::PARAM_ENABLE_AJAX_IFRAME_RESPONSE_MODE] == 1)
             {  
                 header('Content-Type: text/xml');
-                $html = "<?xml version=\"1.0\"?><raw><![CDATA[\n{$html}\n]]></raw>";
+                $html = "<"."?xml version=\"1.0\"?"."><raw><![CDATA[\n{$html}\n]]></raw>";
             }   
 
             print $html;
@@ -212,6 +212,65 @@ class WFRequestController extends WFObject
         return false;
     }
 
+    /**
+     * Determine whether the current request is from a mobile browers
+     *
+     *
+     * @return boolean
+     */
+    function isMobileBrowser()
+    {
+        $op = (isset($_SERVER['HTTP_X_OPERAMINI_PHONE']) ? strtolower($_SERVER['HTTP_X_OPERAMINI_PHONE']) : '');
+        $ua = (isset($_SERVER['HTTP_USER_AGENT']) ? strtolower($_SERVER['HTTP_USER_AGENT']) : '');
+        $ac = (isset($_SERVER['HTTP_ACCEPT']) ? strtolower($_SERVER['HTTP_ACCEPT']) : '');
+
+        $isMobile =    strpos($ac, 'application/vnd.wap.xhtml+xml') !== false
+                    || $op != ''
+                    || strpos($ua, 'sony') !== false 
+                    || strpos($ua, 'symbian') !== false 
+                    || strpos($ua, 'nokia') !== false 
+                    || strpos($ua, 'samsung') !== false 
+                    || strpos($ua, 'mobile') !== false
+                    || strpos($ua, 'windows ce') !== false
+                    || strpos($ua, 'epoc') !== false
+                    || strpos($ua, 'opera mini') !== false
+                    || strpos($ua, 'nitro') !== false
+                    || strpos($ua, 'j2me') !== false
+                    || strpos($ua, 'midp-') !== false
+                    || strpos($ua, 'cldc-') !== false
+                    || strpos($ua, 'netfront') !== false
+                    || strpos($ua, 'mot') !== false
+                    || strpos($ua, 'up.browser') !== false
+                    || strpos($ua, 'up.link') !== false
+                    || strpos($ua, 'audiovox') !== false
+                    || strpos($ua, 'blackberry') !== false
+                    || strpos($ua, 'ericsson,') !== false
+                    || strpos($ua, 'panasonic') !== false
+                    || strpos($ua, 'philips') !== false
+                    || strpos($ua, 'sanyo') !== false
+                    || strpos($ua, 'sharp') !== false
+                    || strpos($ua, 'sie-') !== false
+                    || strpos($ua, 'portalmmm') !== false
+                    || strpos($ua, 'blazer') !== false
+                    || strpos($ua, 'avantgo') !== false
+                    || strpos($ua, 'danger') !== false
+                    || strpos($ua, 'palm') !== false
+                    || strpos($ua, 'series60') !== false
+                    || strpos($ua, 'palmsource') !== false
+                    || strpos($ua, 'pocketpc') !== false
+                    || strpos($ua, 'smartphone') !== false
+                    || strpos($ua, 'rover') !== false
+                    || strpos($ua, 'ipaq') !== false
+                    || strpos($ua, 'au-mic,') !== false
+                    || strpos($ua, 'alcatel') !== false
+                    || strpos($ua, 'ericy') !== false
+                    || strpos($ua, 'up.link') !== false
+                    || strpos($ua, 'vodafone/') !== false
+                    || strpos($ua, 'wap1.') !== false
+                    || strpos($ua, 'wap2.') !== false;
+
+        return $isMobile;
+    }
 
     /**
      *  Get the root {@link WFModuleInvocation} used by the request controller.
