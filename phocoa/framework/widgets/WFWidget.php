75c7774c168b73795799f694193742902429992d
Alan Pinstein <apinstein@mac.com>
2011-02-02 12:20:24 -0500

    switch from print_r to var_export for better readability of debug output.

--- phocoa/framework/widgets/WFWidget.php
+++ phocoa/framework/widgets/WFWidget.php
@@ -383,15 +383,15 @@ abstract class WFWidget extends WFView
         // process value transformer
         if ($binding->valueTransformerName())
         {
-            WFLog::log("Transforming value " . print_r($boundValue, true) . " with " . $binding->valueTransformerName(), WFLog::TRACE_LOG);
+            WFLog::log("Transforming value " . var_export($boundValue, true) . " with " . $binding->valueTransformerName(), WFLog::TRACE_LOG);
             $vt = WFValueTransformer::valueTransformerForName($binding->valueTransformerName());
             $boundValue = $vt->transformedValue($boundValue);
-            WFLog::log("Transformed value: " . print_r($boundValue, true), WFLog::TRACE_LOG);
+            WFLog::log("Transformed value: " . var_export($boundValue, true), WFLog::TRACE_LOG);
         }
 
         if ($binding->formatter())
         {
-            WFLog::log("Formatting value " . print_r($boundValue, true) . " with " . $binding->formatter(), WFLog::TRACE_LOG);
+            WFLog::log("Formatting value " . var_export($boundValue, true) . " with " . $binding->formatter(), WFLog::TRACE_LOG);
             $formatter = $this->page()->module()->valueForKey($binding->formatter());
             // automatically handle formatting of arrays of objects
             if (is_array($boundValue))
@@ -405,10 +405,10 @@ abstract class WFWidget extends WFView
             {
                 $boundValue = $formatter->stringForValue($boundValue);
             }
-            WFLog::log("Formatted value: " . print_r($boundValue, true), WFLog::TRACE_LOG);
+            WFLog::log("Formatted value: " . var_export($boundValue, true), WFLog::TRACE_LOG);
         }
 
-        WFLog::log("Using value " . print_r($boundValue, true) . " for binding '$prop'", WFLog::TRACE_LOG);
+        WFLog::log("Using value " . var_export($boundValue, true) . " for binding '$prop'", WFLog::TRACE_LOG);
         return $boundValue;
     }
 
@@ -547,7 +547,7 @@ abstract class WFWidget extends WFView
                 {
                     $boundValue = $binding->coalescedOption(WFBindingSetup::WFBINDINGSETUP_NULL_PLACEHOLDER);
                 }
-                WFLog::log("FINAL value " . print_r($boundValue, true) . " for binding {$this->id} / $prop...", WFLog::TRACE_LOG);
+                WFLog::log("FINAL value " . var_export($boundValue, true) . " for binding {$this->id} / $prop...", WFLog::TRACE_LOG);
                 $this->setValueForKey($boundValue, $prop);  // must do this to allow accessors to be called!
             } catch (Exception $e) {
                 WFLog::log("Skipping pullBindings for {$this->id} / {$prop} due to exception: {$e->getMessage()}", WFLog::WARN_LOG);
